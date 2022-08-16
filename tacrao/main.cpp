e6a5f9154b40a39e193841476668508cb770ca32
CharlesPhillipsHouston <charles@spaceflightresearch.net>
2022-08-16 16:29:53 -0500

    Initial Commit

---
+++ tacrao/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tacrao
+//
+//  Created by Charles Phillips on 8/16/22.
+//  Copyright © 2022 charles. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
