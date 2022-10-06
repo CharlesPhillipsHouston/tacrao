9a0d93d8c1eaf964edd19f77213dfa55fa471b36
CharlesPhillipsHouston <charles@spaceflightresearch.net>
2022-10-05 19:09:22 -0500

    find files and detect end of files

--- tacrao/main.cpp
+++ tacrao/main.cpp
@@ -1,11 +1,9 @@
-//  main.cpp 26 sep 2022
+//  main.cpp 5 oct 2022
 //  tacrao - this is a practice input, output program
 //
 //  Created by Charles Phillips on 8/16/22.
 //  Copyright © 2022 charles. All rights reserved
-// this version works and creates output
-// now reading .csv file and data is read better
-// finally reads to end of input file!!!
+// heavily modified by mike marston!!!
 
 #include <iostream>
 #include <fstream>
@@ -14,22 +12,40 @@
 
 using namespace std;
 
+string ExtractNextCsv(string searchString, size_t &startPos)
+{
+        //probably should verify that startPos is positive and less than length of searchString
+        string tempString;
+        size_t nextPos = searchString.find(",", startPos); // find a comma in line
+        if(nextPos == string::npos) // end of line reached
+        {
+          tempString = searchString.substr(startPos);
+          startPos = nextPos;
+        }
+        else
+        {
+          size_t length = nextPos - startPos;
+          tempString = searchString.substr(startPos, length );  //extract string from start of string up but not including ,
+          startPos = nextPos+1; //advance startPos past this occurence
+        }
+
+        return tempString;
+
+}
+
 string fair_name = "a"; // first field is fair name, give it an intial value
 string fair_date = "b"; // second field
 string fair_time = "c"; // third field
-string fair_location = "d";
-string fair_city = "not Houston";
-string fair_county = "not Harris";
-string primary_contact_name = "not Charles";
 
-string line, *p;  // used in getline below in line 62
+string line;  // need this???
 
 int main(int argc, const char * argv[])
 {
+     
       string homeDir = getenv("HOME");  // detects if it is on Mini or laptop
     
-    string ifname = homeDir + "/Desktop/common_files/tacroa_data_big.csv";
-    // this works better when you go to the .csv file
+    string ifname = homeDir + "/Desktop/common_files/tacroa_data.txt";
+    
       string ofname = homeDir + "/Desktop/common_files/tacroa_output.txt";
       
       ifstream fin(ifname);
@@ -41,6 +57,7 @@ int main(int argc, const char * argv[])
        cerr << "Error opening input file " << ifname << endl;
        return 1;
      }
+    
    /*
      ofstream fout(outFilePath);
       
@@ -49,6 +66,7 @@ int main(int argc, const char * argv[])
         cerr << "Error opening output file " << outFilePath << endl;
         return 1;
       }
+      
         ifstream targetFile(homeDir + "/Desktop/common_files/satellite1.txt");
     */
     
@@ -56,54 +74,46 @@ int main(int argc, const char * argv[])
     
      int i = 0; // set up the way to count in the while loop
      
-     while (!fin.eof()) 
+     while (i < 4)
      {
      //    string line;  //  create line
          getline(fin, line);
+         fout <<"Line: "  << line << endl;
          
-   //      string fair_name = line.substr(); // read fair name at pos this reads the entire file!!
+        size_t pos = line.find(","); // find a comma in line
+        string fair_name = line.substr(0, pos);  //extract string from start of string up but not including ,
+                      
+        size_t pos1 = line.find(",", pos+1); // find the next comma in line (search starts at pos+1)
+        string fair_date = line.substr (pos+1, pos1-(pos+1)); //extract starting 1st char after last , & up to but not including , just found
+            
+        size_t pos2 = line.find(",", pos1+1); // find next comma in line
+        string fair_time = line.substr (pos1+1, pos2-(pos1+1));
          
-         string fair_name = line.substr(0,29); // just the first ten char first char 0 then 9 more
-         
-    size_t pos = line.find("\t"); // first "position" find a comma in line
-         // pos = position??
-         
-    string fair_date = line.substr (pos +1,29); // get second field nine char
- 
-  // pos and pos1 find the same place
-         
-     size_t pos1 = line.find("\t"); // find another comma, second field
-                 
-    string fair_time = line.substr (pos1 +1,29); // at pos(1)??
-         
-  //  size_t pos2 = line.find(","); // find another comma, second field
-                 
- // string fair_location = line.substr (pos2); // at pos(1)??
-    
          ++i;  // increment i to stop while loop
     
      //    const char *p = line.c_str();
      //    p = strtok(line, ",");
          
- //    const char *s = line.c_str(); // throw in some C here this does work
+ //    const char *s = line.c_str(); // throw in some C here
  //    sscanf(s, "%s" "%s" "%s", &fair_name, &fair_date, &fair_time);
     
     //     fout << "line: " << line << endl; // just output the entire line to see what we got
     // this works great!
          
-    fout << "first field: " << fair_name << '\n' << "second field: " << fair_date << "\n" << "third field: " << fair_time << "\n" << endl;
+  fout << "Name " << fair_name << "\n" << " Fair Date " << fair_date << "\n" << " Fair Time " << fair_time << "\n" << endl;
          
-         // << "Fair Location: " << "\n" << fair_location << endl;
-         
-     //    fout << "Name " << fair_name << '\n' << endl;
-         
-         
-         // " Date " << fair_date << '\n' << " Time " << fair_time << " Location " << fair_location << endl; // output
+      size_t startPos = 0;
+      int counter = 1;
+      while(startPos != string::npos)
+      {
+        string nextString = ExtractNextCsv(line, startPos); //note startPos passed by reference so it can be updated!!!
+        fout << "Var " << counter << " = " << nextString << std::endl;
+        counter++;
+      }
+    }  // end of while loop
     
-     }  // end of while loop
-    
-    fin.close(); // close input stream
-    fout.close(); // close output stream
+    fin.close();
+    fout.close();
     
     return 0;
 }
